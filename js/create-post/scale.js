4208c993c90d0d8b3f35eb6fb6f3c299f8a48e22
Aisylu Iunusova <aisylu.iunusova@gmail.com>
2021-03-13 15:00:07 +0300

    Корректировка по замечаниям наставника

--- js/create-post/scale.js
+++ js/create-post/scale.js
@@ -3,17 +3,20 @@ import { MIN_SCALE, MAX_SCALE, STEP_SCALE } from '../const.js'
 const scaleControlSmaller = document.querySelector('.scale__control--smaller');
 const scaleControlBigger = document.querySelector('.scale__control--bigger');
 const scaleControlValue = document.querySelector('.scale__control--value');
-const postImage = document.querySelector('.img-upload__preview');
+const postImage = document.querySelector('.img-upload__preview img');
 
 let scaleValue = 100;
+
+const scalingImage = (scale) => {
+  scaleControlValue.value = `${scale}%`;
+  postImage.style = `transform:scale(${scale / 100})`;
+};
 
 scaleControlSmaller.addEventListener('click', () => {
   if (scaleValue > MIN_SCALE && scaleValue <= MAX_SCALE) {
     scaleValue -= STEP_SCALE;
   }
-
-  scaleControlValue.value = `${scaleValue}%`;
-  postImage.style = `transform:scale(${scaleValue / 100})`;
+  scalingImage(scaleValue);
 });
 
 
@@ -21,13 +24,11 @@ scaleControlBigger.addEventListener('click', () => {
   if (scaleValue >= MIN_SCALE && scaleValue < MAX_SCALE) {
     scaleValue += STEP_SCALE;
   }
-
-  scaleControlValue.value = `${scaleValue}%`;
-  postImage.style = `transform:scale(${scaleValue / 100})`;
+  scalingImage(scaleValue);
 });
 
 const resetScale = () => {
-  scaleControlValue.value = '100%';
+  scalingImage(100);
   scaleValue = 100;
 };
 
